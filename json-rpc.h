5a42056a794da6f035dc48dde4dd28e5754ad395
jorgdebont <jacbont@student.avans.nl>
2015-01-08 00:39:36 +0100

    cleaned up jsonrpc.h, removed some functions

--- json-rpc.h
+++ json-rpc.h
@@ -45,20 +45,11 @@ struct tuple
     response_t response;
     call_t call;
 };
-
-typedef void (*FunctionCallback)();
-void decode_json_rpc(char *json_string, struct tuple *tup);
-void encode_json_rpc(struct tuple *json_tuple, char *jsonString);
-void json_rpc_handler(call_t *json_call);
-void call_to_string(call_t *json_call, char *json_string);
-void response_to_string(response_t *json_response, char *json_string);
-void set_ip_addr(jsmn_parser *parser, char *ip_addr);
-void set_method(jsmn_parser *parser, char *methodName);
-void set_param(jsmn_parser *parser, char *params);
-void set_result(jsmn_parser *parser, char *results);
-void set_id(jsmn_parser *parser, char *id);
-void print_tokens(char *json_string, jsmntok_t *tokens, size_t array_length);
-
 uint8_t jsoneq(const char *json, jsmntok_t *tok, const char *s);
+void json_rpc_handler(call_t *json_call);
+void encode_json_rpc(struct tuple *json_tuple, char *jsonString);
+void response_to_string(response_t *json_response, char *json_string);
+void call_to_string(call_t *json_call, char *json_string);
+void decode_json_rpc(char *json_string, struct tuple *tup);
 
 #endif /* SRC_JSON_RPC_H_ */
