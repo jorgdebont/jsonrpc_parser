4bc2291fff41b51c13364b64d97f2e46c45385f6
jorgdebont <jacbont@student.avans.nl>
2015-01-08 15:07:14 +0100

    completed array support, should work right now

--- json-rpc.h
+++ json-rpc.h
@@ -51,5 +51,6 @@ void encode_json_rpc(struct tuple *json_tuple, char *jsonString);
 void response_to_string(response_t *json_response, char *json_string);
 void call_to_string(call_t *json_call, char *json_string);
 void decode_json_rpc(char *json_string, struct tuple *tup);
+void get_array_from_tuple(struct tuple *json_tuple, char output_array[][50],uint8_t amount_of_parameters);
 
 #endif /* SRC_JSON_RPC_H_ */
